7dc7a65dd177d16214146d7e52e3e4da8a3e343d
Brendan McGuire <artificalchamber@gmail.com>
2017-10-06 23:38:39 -0400

    docs: Add features tag and about

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * vexdb - An API wrapper for VexDB (https://vexdb.io)
+ * Copyright (c) 2017 Brendan McGuire. See LICENSE file for details (MIT LICENSE)
+ * 
+ * Features of vexdb!
+ *  - Complete Cache Control
+ *  - Promises
+ *  - Isometric
+ *  - Live Subscriptions
+ */
+
 const cache = require("./lib/cache");
 const { request, get, getAll, size } = require("./lib/request");
 const { configure, globalOptions } = require("./lib/configure");
