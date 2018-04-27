853cd50d074331ab79494591e802ab0c55d986c5
MayorMonty <artificalchamber@gmail.com>
2018-04-27 18:08:26 -0400

    More tests, and fetch on command

--- test/live.js
+++ test/live.js
@@ -1,5 +1,5 @@
 import test from "ava";
-import { get, live, size } from "../main";
+import { get, live, size, constant } from "../main";
 import EventEmitter from "events";
 
 function wait(t, v) {
@@ -30,6 +30,7 @@ test(".live() does not prefetch when not told to", async t => {
         sku: "RE-VRC-17-3805"
     });
     matches.on("prefetch", items => t.fail())
+    t.is(matches.current().length, 0);
 });
 
 test(".live() with no prefetch emits `item` the correct number of times", t => {
@@ -48,8 +49,34 @@ test(".live() with no prefetch emits `item` the correct number of times", t => {
     })
 });
 
+test(".live() makes no more polls after .close() is called", async t => {
+    constant.settings.live.pollTime = 1;
+    let matches = live("matches", {
+        scored: 1,
+        sku: "RE-VRC-17-3805"
+    });
+    matches.close();
+    matches.on("fetch", () => t.fail());
+    matches.on("prefetch", () => t.fail());
+});
+
+test(".live() changes parameters successfully", async t => {
+    let inital = 0;
+    let matches = live("matches", {
+        scored: 1,
+        sku: "RE-VRC-17-3805"
+    });
+    matches.fetch()
+        .then(() => {
+            inital = matches.current().length;
+            matches.params({ filter: () => false });
+        })
+        .then(matches.fetch)
+        .then(() => t.is(matches.current().length, inital))
+})
 
 test.afterEach.always(t => {
+    constant.settings.live.pollTime = 10 * 1000;
     let interval = setInterval(() => { }, 9999);
     for (let i = 0; i < interval.length; i++) {
         clearInterval(interval[i]);
