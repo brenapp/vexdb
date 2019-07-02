d39f42dd9053ce1fe03f73d8a5f64a036c98d368
MayorMonty <artificalchamber@gmail.com>
2019-07-01 23:50:05 -0400

    Rename example file

--- example.ts
+++ example.ts
@@ -1,11 +1,24 @@
 import * as vexdb from "./src/main";
+import filter from "./src/util/filter";
 
 async function main() {
   let teams = await vexdb.get("teams", {
-    number: /^[0-9]{4}B/
+    number: number => number.startsWith("8686")
   });
 
-  console.log(teams);
+  let awards = await vexdb.get("awards", {
+    name: "Tournament Champions (VRC/VEXU)",
+    team: teams.map(a => a.number)
+  });
+
+  console.log("THE 8686 ALLIANCE");
+  teams.forEach(team => {
+    console.log(
+      `${team.number} - ${team.team_name} (${team.city}, ${team.region})`
+    );
+  });
+
+  console.log(awards.length);
 }
 
 main();
