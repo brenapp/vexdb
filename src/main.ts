5686d69b7765eb9d1ad7d63eebb13b667b9d4df9
Brendan McGuire <brendan@bren.app>
2020-04-27 03:13:26 -0400

    export constants

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import request from "./methods/request";
 import * as cache from "./methods/cache";
 import get from "./methods/get";
+import size from "./methods/size";
+import live from "./methods/live";
 
-export { request, cache, get };
+import * as req from "./constants/RequestObjects";
+import * as res from "./constants/ResponseObjects";
+import * as settings from "./constants/settings";
+const constant = { ...req, ...res, ...settings };
+
+export { request, cache, get, size, live, constant };
