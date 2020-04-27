cb8b61f18859815724dc33f5b3e3147adce9041a
Brendan McGuire <brendan@bren.app>
2020-04-27 03:13:18 -0400

    formatting

--- test/configure.js
+++ test/configure.js
@@ -2,19 +2,19 @@ import test from "ava";
 import { size, constant } from "../out/main";
 import { version } from "../package";
 
-test("Register a header", t => {
+test("Register a header", (t) => {
   constant.header({
-    Accept: "application/json"
+    Accept: "application/json",
   });
   t.deepEqual(constant.settings.headers, {
     "User-Agent": "vexdb (nodejs)",
-    Accept: "application/json"
+    Accept: "application/json",
   });
 });
 
-test("Register a default parameter", async t => {
+test("Register a default parameter", async (t) => {
   constant.param({
-    season: "Nothing But Net"
+    season: "Nothing But Net",
   });
   t.is(await size("events"), 1044);
 });
